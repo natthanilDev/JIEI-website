d49eaabbb6ef5036aec43967a01876a6fd5a7810
Natthanil Wannasopa <J2505001@JIEI.local>
2025-06-02 16:56:04 +0700

    'upload'

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import './loading.css';
+
+export default function Loading() {
+
+  return (
+    <div className="loading-box">
+      <div className="loading"></div>
+      <p className='loadingText'>Loading... </p>
+    </div>
+  );
+}
